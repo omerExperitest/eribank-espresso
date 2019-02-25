9f8d3f30485e8c0275d3c72104bd35df116f5225
Omer Madi <omer@experiteset.com>
2019-02-26 01:15:03 +0200

    added mexico

--- app/src/main/java/com/experitest/ExperiBank/CountryListActivity.java
+++ app/src/main/java/com/experitest/ExperiBank/CountryListActivity.java
@@ -16,7 +16,7 @@ public class CountryListActivity extends Activity {
 	ListView countryListView;
 	private ArrayAdapter<String> listAdapter;
 	private String[] countriesArray = new String[] { "India", "USA", "Iceland", "Greenland", "Switzerland", "Norway", "New Zealand", "Greece", "Italy", "Ireland", "China", "Japan", "France", "Russia", "Australlia", "Canada", "Brazil", "Indonesia",
-			"Nigeria", "Bangladesh", "Philippines", "Vietnam", "Ethiopia", "Egypt", "Germany", "Iran", "Turkey", "Thailand", "United Kingdom", "South Africa", "South Korea", "Myanmar", "Spain", "Colombia", "Ukraine", "Tanzania",
+			"Nigeria", "Bangladesh","Mexico", "Philippines", "Vietnam", "Ethiopia", "Egypt", "Germany", "Iran", "Turkey", "Thailand", "United Kingdom", "South Africa", "South Korea", "Myanmar", "Spain", "Colombia", "Ukraine", "Tanzania",
 			"Argentina" };
 
 
